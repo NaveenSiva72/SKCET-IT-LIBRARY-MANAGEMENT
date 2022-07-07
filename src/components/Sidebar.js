5860a02a36e5db328e663c0cd567bfcfadd56ef6
NaveenSiva72 <naveensiva72@gmail.com>
2022-07-07 22:01:58 +0530

    sidebar and navbar without font change

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,139 +1,76 @@
 import React from "react";
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 const Sidebar = () => {
   return (
-    <div class="container-fluid">
-      <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-          <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-            <a
-              href="/"
-              class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none"
-            >
-              <span class="fs-5 d-none d-sm-inline">Menu</span>
-            </a>
+    <div className="container-fluid ">
+      <div className="row flex-nowrap">
+        <div className="col-auto col-md-3 col-xl-2  px-0 bg-dark">
+          <div className="d-flex flex-column align-items-start align-items-sm-start px-3 pt-2 text-white min-vh-100">
             <ul
-              class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+              className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
               id="menu"
             >
-              <li class="nav-item">
-                <a href="#" class="nav-link align-middle px-0">
-                  <i class="fs-4 bi-house"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Home</span>
+              <li className="nav-item">
+                <a href="#" className="navbar-brand text-light mt-5 mb-auto">
+                  <i className="fs-4  bi-house"></i>{" "}
+                  <span className=" d-none d-sm-inline ">Home</span>
                 </a>
               </li>
               <li>
                 <a
                   href="#submenu1"
                   data-bs-toggle="collapse"
-                  class="nav-link px-0 align-middle"
+                  className="navbar-brand text-light mt-5"
                 >
-                  <i class="fs-4 bi-speedometer2"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Dashboard</span>{" "}
+                  <i className="fs-4 bi-speedometer2"></i>{" "}
+                  <span className="d-sm-inline">Dashboard</span>{" "}
                 </a>
                 <ul
-                  class="collapse show nav flex-column ms-1"
+                  className="collapse show nav flex-column ms-1"
                   id="submenu1"
                   data-bs-parent="#menu"
                 >
-                  <li class="w-100">
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Item</span> 1{" "}
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Item</span> 2{" "}
-                    </a>
-                  </li>
                 </ul>
               </li>
               <li>
-                <a href="#" class="nav-link px-0 align-middle">
-                  <i class="fs-4 bi-table"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Orders</span>
+                <a href="#" className="navbar-brand text-light mt-5">
+                  <i className="fs-5 bi-table"></i>{" "}
+                  <span className="d-none d-sm-inline">Orders</span>
                 </a>
               </li>
               <li>
                 <a
                   href="#submenu2"
                   data-bs-toggle="collapse"
-                  class="nav-link px-0 align-middle "
+                  className="navbar-brand text-light mt-5 "
                 >
-                  <i class="fs-4 bi-bootstrap"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Bootstrap</span>
+                  <i className="fs-4 bi-bootstrap"></i>{" "}
+                  <span className="d-none d-sm-inline">Bootstrap</span>
                 </a>
-                <ul
-                  class="collapse nav flex-column ms-1"
-                  id="submenu2"
-                  data-bs-parent="#menu"
-                >
-                  <li class="w-100">
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Item</span> 1
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Item</span> 2
-                    </a>
-                  </li>
-                </ul>
               </li>
               <li>
                 <a
                   href="#submenu3"
                   data-bs-toggle="collapse"
-                  class="nav-link px-0 align-middle"
+                  className="navbar-brand text-light mt-5"
                 >
-                  <i class="fs-4 bi-grid"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Products</span>{" "}
+                  <i className="fs-4 bi-grid"></i>{" "}
+                  <span className="d-none d-sm-inline">Products</span>{" "}
                 </a>
-                <ul
-                  class="collapse nav flex-column ms-1"
-                  id="submenu3"
-                  data-bs-parent="#menu"
-                >
-                  <li class="w-100">
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Product</span> 1
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Product</span> 2
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Product</span> 3
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Product</span> 4
-                    </a>
-                  </li>
-                </ul>
+               
               </li>
               <li>
-                <a href="#" class="nav-link px-0 align-middle">
-                  <i class="fs-4 bi-people"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Customers</span>{" "}
+                <a href="#" className="navbar-brand text-light mt-5">
+                  <i className="fs-4 bi-people"></i>{" "}
+                  <span className="d-none d-sm-inline">Customers</span>{" "}
                 </a>
               </li>
             </ul>
           </div>
         </div>
-        <div class="col py-3">Content area...</div>
+        <div className="col py-3">Content area...</div>
       </div>
     </div>
   );
