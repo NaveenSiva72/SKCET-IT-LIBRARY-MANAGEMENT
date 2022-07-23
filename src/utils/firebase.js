27004d0d0b0358a47f90222014b7fdee4a7fb1ba
NaveenSiva72 <naveensiva72@gmail.com>
2022-07-23 22:42:50 +0530

    firebase updated

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,14 +7,14 @@ import {getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpwry05BEK2T5e_WhSePil2QJght1kh_w",
-  authDomain: "skcet-it-library-managem-3df37.firebaseapp.com",
-  databaseURL: "https://skcet-it-library-managem-3df37-default-rtdb.firebaseio.com",
-  projectId: "skcet-it-library-managem-3df37",
-  storageBucket: "skcet-it-library-managem-3df37.appspot.com",
-  messagingSenderId: "945967509495",
-  appId: "1:945967509495:web:7fcfe922600a9497be08f2",
-  measurementId: "G-RDS8N1LMSD"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
      };
 const app = initializeApp(firebaseConfig);
 const auth =getAuth(app)
