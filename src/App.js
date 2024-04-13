b4a4cd7c2776a094d78ba96af59e0aac56aed6c4
rahulms02 <rahulms02@gmail.com>
2024-04-13 17:14:51 +0530

    added signup page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import React , {useState,useEffect} from 'react';
 import Mainpage from './components/Mainpage';
 import Login from './components/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-
+import SignUP from './components/SignUP';
 function App() {
-  
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/Dashboard" element={<Mainpage />}  />
           <Route path="/" element={<Login />} /> 
+          <Route path="/SignUp" element={<SignUP />} /> 
         </Routes> 
     </BrowserRouter>
+    
   );
 }
 
